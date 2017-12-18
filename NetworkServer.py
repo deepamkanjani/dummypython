238448ec6ee7c26078baa400625a419dfe7d746b
Deepam <deepamkanjani@gmail.com>
2017-12-18 10:25:01 +0530

    Few Modifications and Adding Banner Grabbing Code

---
+++ NetworkServer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+__author__='deepam'
+
+import socket
+
+size = 512
+host = ''
+port = 9898
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+sock.bind((host,port))
+sock.listen(5)
+
+c, addr = sock.accept()
+data = c.recv(size)
+
+if data:
+	f = open("storage.dat", 'w')
+	print("conection from: ", addr[0])
+	f.write(addr[0])
+	f.write(":")
+	f.write(data.decode("utf-8"))
+	f.close()
+sock.close()
--- testscript.py
+++ testscript.py
@@ -4,6 +4,8 @@ __author__='deepam'
 import argparse
 import os
 from subprocess import call
+import socket
+import re
 
 class linux:
 
@@ -52,8 +54,47 @@ newTestData=["redhat1","UNIX"]
 for i in newTestData:
 	if(linux(i).getVal() == "unix"):
 			print(os.getenv("PATH"))
-			os.system("ls -la")
-			call(["ls","-la"])
+			#os.system("ls -la")
+			#call(["ls","-la"])
 
+
+print(socket.gethostbyaddr("8.8.8.8"))
+print(socket.gethostbyname("www.google.com"))
+
+
+mysock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#addr=('localhost',5555)
+#mysock.connect(addr)
+
+try:
+	msg="hi this is a test\n"
+#	mysock.sendall(msg)
+except sock.errno:
+	print("Socket Error")
+finally:
+	mysock.close()
+
+# try banner grabbing
+
+sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock1.connect(("www.microsoft.com", 80))
+
+http_get = "GET / HTTP/1.1\nHost: www.microsoft.com\n\n"
+data = ''
+try:
+	sock1.sendall(http_get)
+	data=sock1.recvfrom(1024)
+except:
+	print("Socket error", socket.errno)
+finally:
+	print("Closing Connection")
+	sock1.close()
+strdata=data[0].decode("utf-8")
+headers=strdata.splitlines()
+
+for s in headers:
+	if re.search('Server:', s):
+		s = s.replace("Server: ", "")
+		print(s)
 
 
